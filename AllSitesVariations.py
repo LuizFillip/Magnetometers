07bf2bdaabefef2e7abdb90e9d2abcb02797c74d
Luiz Fillip <luizfillip@outlook.com>
2022-03-14 15:18:54 -0300

    alterações

--- AllSitesVariations.py
+++ AllSitesVariations.py
@@ -21,16 +21,14 @@ from MagnetometersAnalysis import *
 
 # Select area for to shade (analysis)
 tm1 = datetime.datetime(2022, 1, 15, 13, 0)
-tm2 = datetime.datetime(2022, 1, 15, 17, 0)
+tm2 = datetime.datetime(2022, 1, 15, 19, 0)
 
 
 component = 'H(nT)'
 limit = 12
 N = 10
 
-
-infile = 'G:\\My Drive\\Python\\doctorate-master\\'\
-        'AtmospherePhysics\\Database\\'
+infile = 'G:\\My Drive\\Python\\doctorate-master\\MagnetometerAnalysis\\Database\\'
 folder = 'Magnetometer15012022\\'
 
 #Get sites informations from array in the file
@@ -54,40 +52,27 @@ for ax, num in zip(axs.flat, range(len(acc))):
     df = setting_dataframe(infile + folder, 
                            filename)
     
-    # Setting limits
-    #ax.set(ylim = [-limit, limit], xlabel = 'Local time',
-    #       xlim = [df.index[0], df.index[-1]])
-    
     # Plot Subract from running average 
-    df[component].plot(ax = ax, color = 'k', lw = 1)
-    df[component].rolling(window = N).mean().plot(linestyle = ':')
-    
-    ax1 = ax.twinx()
-    
-    # Plot vertical component in the right axes
-    vertical, = ax1.plot(df['Z(nT)'], lw = 1)
-    
-    setting_axes(ax1, color = vertical.get_color(), position = 'right')
-    
+    df[component].plot(ax = ax, color = 'k', lw = 1, label = 'data')
+    df[component].rolling(window = N).mean().plot(ax = ax,
+                                                  label = f'running average - {N} min',
+                                                  lw= 2, linestyle = ':')
+
     #Put the name of location
     ax.text(0.03, 0.8, names[num], 
             transform = ax.transAxes)
     
-
-    if num == 0:    
-        ax.spines['bottom'].set_visible(False)       
-    elif num == (len(acc) - 1):    
-        ax.spines['top'].set_visible(False)
-    else:
-        ax.spines['top'].set_visible(False)
-        ax.spines['bottom'].set_visible(False)
-    
+    remove_lines(ax, acc, num)
     
     ax.axvspan(tm1, tm2, color='lightgrey', alpha=0.4, lw=0)
     
     ax.xaxis.set_major_formatter(dates.DateFormatter('%H'))
     ax.xaxis.set_major_locator(dates.HourLocator(interval = 2))
     
+    ax.set(xlabel = 'Time (hours)')
+    
+    
+ax.legend(loc = 'center', bbox_to_anchor=(0.5, 10.3), ncol = 2)
 if component == 'H(nT)':
     ylabel = 'Horizontal'
 elif component == 'Z(nT)':
@@ -96,18 +81,16 @@ elif component == 'F(nT)':
     ylabel = 'Total'
 
 
-
-fig.text(0.04, 0.5, f'Horizontal component (nT)', va='center', 
+fig.text(-0.01, 0.5, f'Horizontal component (nT)', va='center', 
              rotation='vertical', fontsize = fontsize)   
 
-fig.text(0.98, 0.5, f'Vertical component (nT)', va='center', 
-             rotation='vertical', fontsize = fontsize, 
-             color = vertical.get_color()) 
-
-
-fig.suptitle(f'EMBRACE Magnetometers Network\n dTrend - {tm2.date()}', 
-             y = 0.93)
+fig.suptitle(f'Hourly variation of\n horizontal component - {tm2.date()}', 
+             y = 0.97)
 
 plt.rcParams.update({'font.size': 12})    
 
-NameToSave = f'{ylabel}1512022dtrend.png'
+NameToSave = f'{ylabel}Component{tm1.strftime("%d%m%Y")}AllSites.png'
+
+save_plot(NameToSave, dpi = 100)
+plt.show()  
+    
